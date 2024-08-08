23be10664b7b5c46346665eb8b2fde09f0a7c4bc
Sam Spencer <sam.spencer@microsoft.com>
2024-08-08 12:00:05 -0700

    Added a comment to provide more context

--- ChatApp.WebApi/SemanticKernelTelemetryExtensions.cs
+++ ChatApp.WebApi/SemanticKernelTelemetryExtensions.cs
@@ -3,6 +3,13 @@ using OpenTelemetry.Trace;
 
 namespace ChatApp.WebApi
 {
+    // Adds OpenTelemetry support for the Semantic Kernel.
+    // As the semantic conventions for gen_ai telemetry are still experimental,
+    // it needs to be enabled with an App Context switch.
+    // Sensitive mode will add the chat messages to the telemetry, which is useful
+    // for debugging but should not be used in production.
+    // For more details see https://github.com/microsoft/semantic-kernel/blob/main/dotnet/samples/Demos/TelemetryWithAppInsights/README.md
+
     public static class SemanticKernelTelemetryExtensions
     {
         public static IHostApplicationBuilder AddSemanticKernelTelemetry(this IHostApplicationBuilder builder)
