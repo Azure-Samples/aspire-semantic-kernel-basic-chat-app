868e687fb49b1ac0881070742cf3e7f2dd4d724e
Sam Spencer <sam.spencer@microsoft.com>
2024-08-08 11:49:00 -0700

    Adds OpenTelemetry for Semantic Kernel so the full details are shown in the Aspire Dashboard

---
+++ ChatApp.WebApi/SemanticKernelTelemetryExtensions.cs
@@ -0,0 +1,36 @@
+﻿using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+namespace ChatApp.WebApi
+{
+    public static class SemanticKernelTelemetryExtensions
+    {
+        public static IHostApplicationBuilder AddSemanticKernelTelemetry(this IHostApplicationBuilder builder)
+        {
+            if (builder.Environment.IsDevelopment())
+            {
+                AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+            }
+            else
+            {
+                AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnostics", true);
+            }
+
+
+            builder.Services.AddOpenTelemetry()
+            .WithMetrics(metrics =>
+            {
+                metrics.AddMeter("Microsoft.SemanticKernel*")
+                   .AddMeter("Azure.*");
+            })
+            .WithTracing(tracing =>
+            {
+                tracing.AddSource("Microsoft.SemanticKernel.*")
+                .AddSource("Azure.*")
+               .AddSource("Microsoft.ML.*");
+            });
+
+            return builder;
+        }
+    }
+}
